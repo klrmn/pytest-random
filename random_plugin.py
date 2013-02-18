c3f5e96d1c5d1d9affbefd2097b0d0715d8e2d9d
Florian Schulze <florian.schulze@gmx.net>
2013-02-18 12:52:43 +0100

    Make test runs repeatable by printing the seed and adding an option to set it.

--- random_plugin.py
+++ random_plugin.py
@@ -1,4 +1,6 @@
-import pytest, random
+from random import Random
+import time
+
 
 # command line options
 def pytest_addoption(parser):
@@ -8,6 +10,17 @@ def pytest_addoption(parser):
         dest="random",
         default=False,
         help="randomize the tests to be run. defaults to False.")
+    group._addoption('--random-seed',
+        action="store",
+        dest="random_seed",
+        type=int,
+        default=int(time.time() * 256),
+        help="the seed to use for randomization if you need to repeat a run.")
+
+
+def pytest_report_header(config):
+    if config.option.random:
+        return "Tests are shuffled using seed number %d." % config.option.random_seed
 
 
 def pytest_collection_modifyitems(session, config, items):
@@ -15,5 +28,6 @@ def pytest_collection_modifyitems(session, config, items):
     the items in-place."""
     if not config.option.random:
         return
-
+    random = Random()
+    random.seed(config.option.random_seed)
     items = random.shuffle(items)
